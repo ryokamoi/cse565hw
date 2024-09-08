f75ac88b31e8da3e3749291c0f44da428109abdc
ryokamoi <ryokamoi.jp@gmail.com>
2024-09-07 23:31:15 -0400

    Runtime error on test 71

--- hw1_q9.py
+++ hw1_q9.py
@@ -7,61 +7,60 @@ junctions_cost = [int(j) for j in input().split()]
 road_num = int(input())
 roads_list = []
 for _ in range(road_num):
-    e_from, e_to = [int(j) - 1 for j in input().split()]
-    roads_list.append((e_from, e_to))
+    v_from, v_to = [int(j) - 1 for j in input().split()]
+    roads_list.append((v_from, v_to))
 
 # construct a graph (adjacent list)
 graph: dict[int, list[int]] = {}
+inverse_graph: dict[int, list[int]] = {}
 for jnc in range(num_junction):
     graph[jnc] = []
+    inverse_graph[jnc] = []
 
 for i in range(road_num):
-    e_from, e_to = roads_list[i]
-    graph[e_from].append(e_to)
+    v_from, v_to = roads_list[i]
+    graph[v_from].append(v_to)
+    inverse_graph[v_to].append(v_from)
 
 
-def explore(graph, v, color):
+def explore(graph, v, color, ordered_vertices: list[int]):
+    global color_list
     color_list[v] = color
     
-    for edge in graph[v]:
-        if color_list[edge] is None:
-            explore(graph, edge, color)
+    for to_v in graph[v]:
+        if color_list[to_v] is None:
+            ordered_vertices = explore(graph, to_v, color, ordered_vertices)
     
-    global ordered_vertices
-    ordered_vertices = [v] + ordered_vertices
+    ordered_vertices.append(v)
+    return ordered_vertices
 
 
 def dfs(graph: dict[int, list[int]], vertices: List[int]):
+    ordered_vertices = []
+    
     global color_list
     color: int = 1
     for v in vertices:
         if color_list[v] is None:
-            explore(graph, v, color)
+            ordered_vertices = explore(graph, v, color, ordered_vertices)
             color += 1
+    
+    return ordered_vertices[::-1]
 
 
 # We need to make a police checkpoint in each SCC
 # create inverse graph to get order of vertices
-inverse_graph: dict[int, list[int]] = {}
-for v in range(num_junction):
-    inverse_graph[v] = []
-
-for v, edges in graph.items():
-    for to_v in edges:
-        inverse_graph[to_v].append(v)
-
 color_list = [None for _ in range(num_junction)]
-ordered_vertices = []
-dfs(inverse_graph, list(range(num_junction)))
+order = dfs(inverse_graph, list(range(num_junction)))
 
 # SCC algorithm
 color_list = [None for _ in range(num_junction)]
-dfs(graph, ordered_vertices)
+_ = dfs(graph, order)
 
 price = 0
 combinations = 1
 
-scc_cost_dict: dict[str, list] = {}
+scc_cost_dict: dict[int, list] = {}
 for color, cost in zip(color_list, junctions_cost):
     scc_cost_dict.setdefault(color, []).append(cost)
 
