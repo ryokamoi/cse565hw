0b8a129ec657e8437c99fe25f6251901ca65a865
ryokamoi <ryokamoi.jp@gmail.com>
2024-09-28 16:17:57 -0400

    updated hw2/hw2_q10.cpp, still Time limit exceeded on test 5

--- hw2/hw2_q10.cpp
+++ hw2/hw2_q10.cpp
@@ -1,67 +1,68 @@
 #include <iostream>
 #include <vector>
 #include <cmath>
+
 using namespace std;
-
-const int MAX_NUM = 5000000;
-
-// Function to count prime factors of a number
-int count_prime_factors(int num, const vector<int>& prime_numbers, const vector<bool>& is_prime, vector<int>& num_prime_factors) {
-    if (num <= 1) return 0;
-    if (is_prime[num]) return 1;
-
-    for (int prime : prime_numbers) {
-        if (num % prime == 0) {
-            return 1 + num_prime_factors[num / prime];
-        }
-    }
-    return 0;
-}
 
 int main() {
     int t;
     cin >> t;
 
     vector<pair<int, int>> problems(t);
-    for (int i = 0; i < t; i++) {
+    for (int i = 0; i < t; ++i) {
         int a, b;
         cin >> a >> b;
         problems[i] = make_pair(a, b);
     }
 
-    // Get all prime numbers up to MAX_NUM
-    vector<bool> is_prime(MAX_NUM + 1, true);
-    is_prime[0] = false;
-    is_prime[1] = false;
+    // Set the maximum number
+    int max_num = 5000000;
 
-    for (int i = 2; i <= sqrt(MAX_NUM); i++) {
+    // Sieve of Eratosthenes to mark prime numbers
+    vector<bool> is_prime(max_num + 1, true);
+    is_prime[0] = is_prime[1] = false;
+    for (int i = 2; i * i <= max_num; ++i) {
         if (is_prime[i]) {
-            for (int j = i * i; j <= MAX_NUM; j += i) {
+            for (int j = i * i; j <= max_num; j += i) {
                 is_prime[j] = false;
             }
         }
     }
 
+    // Get all prime numbers up to max_num
     vector<int> prime_numbers;
-    for (int i = 2; i <= MAX_NUM; i++) {
+    for (int i = 2; i <= max_num; ++i) {
         if (is_prime[i]) {
             prime_numbers.push_back(i);
         }
     }
 
-    // Sum of all prime factors up to MAX_NUM
-    vector<int> num_prime_factors(MAX_NUM + 1, 0);
-    vector<int> sum_of_prime_factors(MAX_NUM + 1, 0);
+    // Count number of prime factors
+    vector<int> num_prime_factors(max_num + 1, 0);
+    for (int a = 2; a <= max_num; ++a) {
+        if (is_prime[a]) {
+            num_prime_factors[a] = 1;
+            continue;
+        }
 
-    for (int a = 2; a <= MAX_NUM; a++) {
-        num_prime_factors[a] = count_prime_factors(a, prime_numbers, is_prime, num_prime_factors);
+        for (int prime : prime_numbers) {
+            if (a % prime == 0) {
+                num_prime_factors[a] = 1 + num_prime_factors[a / prime];
+                break;
+            }
+        }
+    }
+
+    // Sum of all prime factors up to max_num
+    vector<long long> sum_of_prime_factors(max_num + 1, 0);
+    for (int a = 2; a <= max_num; ++a) {
         sum_of_prime_factors[a] = sum_of_prime_factors[a - 1] + num_prime_factors[a];
     }
 
-    // Output the results for each problem
-    for (const auto& problem : problems) {
-        int a = problem.first;
-        int b = problem.second;
+    // Answer the queries
+    for (const auto &p : problems) {
+        int a = p.first;
+        int b = p.second;
         cout << sum_of_prime_factors[a] - sum_of_prime_factors[b] << endl;
     }
 
--- hw2/hw2_q10.py
+++ hw2/hw2_q10.py
@@ -18,23 +18,22 @@ for i in range(2, int(max_num ** 0.5) + 1):
 # get all prime numbers up to max_num
 prime_numbers = [i for i in range(max_num + 1) if is_prime[i]]
 
-# sum of all prime factors up to max_num
+# count num of prime factors
 num_prime_factors = [0 for _ in range(max_num + 1)]
-def count_prime_factors(num):
-    if num <= 1:
-        return 0
-    
-    if is_prime[num]:
-        return 1
+for a in range(2, max_num + 1):
+    if is_prime[a]:
+        num_prime_factors[a] = 1
+        continue
     
     for prime in prime_numbers:
-        if num % prime == 0:
-            return 1 + num_prime_factors[num // prime]
+        if a % prime == 0:
+            num_prime_factors[a] = 1 + num_prime_factors[a // prime]
+            break
 
 
+# sum of all prime factors up to max_num
 sum_of_prime_factors = [0 for _ in range(max_num + 1)]
 for a in range(2, max_num + 1):
-    num_prime_factors[a] = count_prime_factors(a)
     sum_of_prime_factors[a] = sum_of_prime_factors[a - 1] + num_prime_factors[a]
 
 # print([sum_of_prime_factors[i] for i in range(10)])
