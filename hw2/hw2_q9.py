735ccbe0de6fa50b4254894a5d30cfdd523671a8
ryokamoi <ryokamoi.jp@gmail.com>
2024-09-28 15:13:03 -0400

    updated hw2/hw2_q9.py, accepted

--- hw2/hw2_q9.py
+++ hw2/hw2_q9.py
@@ -1,29 +1,45 @@
 n = int(input())
 
-time = []
+time = [0]
 for _ in range(n):
     time.append(int(input()) + 1)  # 1-based
 
-time_bool = [False for _ in range(max(time)+1)]
-for t in time:
-    time_bool[t] = True
-
 # optimal answer for each time
-dp = [0 for _ in range(max(time)+1)]
+dp = [0 for _ in range(len(time) + 1)]
 
 
-for t in range(max(time)+1):
-    if not time_bool[t]:
-        if t >= 1:
-            dp[t] = dp[t-1]
-        continue
-    
+def binary_search_index(target_time):
+    left = 0
+    right = len(time) - 1
+    possible_answer = 0
+
+    # if there is no target_time in time, return the index of the largest time smaller than target_time
+    while left <= right:
+        mid = (left + right) // 2
+        
+        if time[mid] == target_time:
+            return mid
+        
+        if time[mid] < target_time:
+            possible_answer = mid
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    return possible_answer
+
+
+for i in range(1, len(time)):
     # print("time", t)
     
-    dp[t] = dp[t-1] + 20
-    dp[t] = min(dp[t], dp[t-min(90, t)] + 50)
-    dp[t] = min(dp[t], dp[t-min(1440, t)] + 120)
+    dp[i] = dp[i-1] + 20
     
-    print(dp[t] - dp[t-1])
-
-# print(dp)
+    ninety_min_idx = binary_search_index(max(0, time[i]-90))
+    # print("time", time[i])
+    # print("ninety_min_idx", ninety_min_idx, time[ninety_min_idx])
+    dp[i] = min(dp[i], dp[ninety_min_idx] + 50)
+    
+    oneday_idx = binary_search_index(max(0, time[i]-1440))
+    dp[i] = min(dp[i], dp[oneday_idx] + 120)
+    
+    print(dp[i] - dp[i-1])
