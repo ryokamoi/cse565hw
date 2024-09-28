eb6b06f9daaa29bf6ca28382f830401ff00aa307
ryokamoi <ryokamoi.jp@gmail.com>
2024-09-28 14:35:31 -0400

    updated hw2/hw2_q9.py, Memory limit exceeded on test 16

--- hw2/hw2_q9.py
+++ hw2/hw2_q9.py
@@ -2,90 +2,28 @@ n = int(input())
 
 time = []
 for _ in range(n):
-    time.append(int(input()))
+    time.append(int(input()) + 1)  # 1-based
 
-ninety_mins_start = 0
-using_ninety_mins_ticket = False
-payment_within_ninety_mins = 0
-oneday_start = 0
-using_oneday_ticket = False
-payment_within_oneday = 0
+time_bool = [False for _ in range(max(time)+1)]
+for t in time:
+    time_bool[t] = True
 
-for i in range(n):
-    # print("90:", ninety_mins_start)
-    # print("oneday:", oneday_start)
-    # print("time:", time[i])
-    
-    if i == 0:
-        print(20)
-        payment_within_oneday += 20
-        payment_within_ninety_mins += 20
+# optimal answer for each time
+dp = [0 for _ in range(max(time)+1)]
+
+
+for t in range(max(time)+1):
+    if not time_bool[t]:
+        if t >= 1:
+            dp[t] = dp[t-1]
         continue
     
-    if using_oneday_ticket:
-        if time[oneday_start] + 1439 >= time[i]:
-            print(0)
-        else:  # expired
-            print(20)
-            using_oneday_ticket = False
-            oneday_start = i
-            ninety_mins_start = i
-            payment_within_oneday = 20
-            payment_within_ninety_mins = 20
-        
-        continue
+    # print("time", t)
     
-    if using_ninety_mins_ticket:
-        if time[ninety_mins_start] + 89 >= time[i]:
-            print(0)
-        else:  # expired
-            using_ninety_mins_ticket = False
-            ninety_mins_start = i
-            payment_within_ninety_mins = 0
-            
-            if time[oneday_start] + 1439 >= time[i]:
-                if payment_within_oneday >= 100:  # get one day ticket
-                    print(120 - payment_within_oneday)
-                    payment_within_oneday = 0
-                    payment_within_ninety_mins = 0
-                else:  # don't get one day ticket
-                    print(20)
-                    payment_within_oneday += 20
-                    payment_within_ninety_mins += 20
-            else:  # cannot buy one day ticket
-                print(20)
-                oneday_start = i
-                payment_within_oneday = 20
-                payment_within_ninety_mins = 20
-        
-        continue
+    dp[t] = dp[t-1] + 20
+    dp[t] = min(dp[t], dp[t-min(90, t)] + 50)
+    dp[t] = min(dp[t], dp[t-min(1440, t)] + 120)
     
-    # not using any ticket
-    # get one day ticket?
-    if time[oneday_start] + 1439 >= time[i]:
-        if payment_within_oneday >= 100:
-            print(120 - payment_within_oneday)
-            using_oneday_ticket = True
-            payment_within_oneday = 0
-            continue
-    else:
-        oneday_start = i
-        payment_within_oneday = 0
-    
-    # get three hour ticket?
-    if time[ninety_mins_start] + 89 >= time[i]:
-        if payment_within_ninety_mins > 30:
-            payment = 50 - payment_within_ninety_mins
-            print(payment)
-            using_ninety_mins_ticket = True
-            payment_within_ninety_mins = 0
-            payment_within_oneday += payment
-            continue
-    else:
-        ninety_mins_start = i
-        payment_within_ninety_mins = 0
-    
-    # not using any ticket
-    print(20)
-    payment_within_oneday += 20
-    payment_within_ninety_mins += 20
+    print(dp[t] - dp[t-1])
+
+# print(dp)
