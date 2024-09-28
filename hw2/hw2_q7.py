b4f9092635c69c4a8b7eb9bcfd82780af6a96619
ryokamoi <ryokamoi.jp@gmail.com>
2024-09-27 22:22:13 -0400

    updated hw2_q7.py accepted

--- hw2/hw2_q7.py
+++ hw2/hw2_q7.py
@@ -1,36 +1,56 @@
+import math
+
+mod = 10 ** 9 + 7
+
 n = int(input())
 a = list(map(int, input().split()))
 
 a = [0] + a  # 1-indexed
 
-# c[j]: the number of sequences where length is i, i-th value is a_k where k <= j
-# initialize n+1 list with 0
+# c[i]: the number of sequences where length is i, i-th value is a_k where k <= j
 c = [0 for _ in range(n + 1)]
-c_prev = [0 for _ in range(n + 1)]
 
-# initialization
-for j in range(1, n + 1):
-    c_prev[j] = c_prev[j - 1] + 1
+# initialization: j=1
+c[0] = 1
+c[1] = 1
 
 # DP - Memory error
-# for i in range(2, n + 1):
-#     for j in range(1, n + 1):
+# for j in range(2, n + 1):
+#     for i in range(1, n + 1):
 #         if a[j] % i != 0:  # not divisible by i
 #             c[i][j] = c[i][j - 1]
 #         else:
 #             c[i][j] = c[i][j - 1] + c[i - 1][j - 1]
 
-# DP
-ans = c_prev[n]
-for i in range(2, n + 1):
-    for j in range(1, n + 1):
-        if a[j] % i != 0:  # not divisible by i
-            c[j] = c[j - 1]
-        else:
-            c[j] = c[j - 1] + c_prev[j - 1]
+# # DP -- TLE
+# for j in range(2, n + 1):
+#     for i in range(n, 0, -1):
+#         if a[j] % i != 0:  # not divisible by i
+#             c[i] = c[i]  # c[i][j] = c[i][j - 1]
+#         else:
+#             c[i] = (c[i] + c[i-1]) % mod
 
-    ans += c[n]
-    for j in range(1, n + 1):
-        c_prev[j] = c[j]
+
+def find_divisors(n):
+    small_divisors = []
+    large_divisors = []
+    for i in range(1, int(math.sqrt(n)) + 1):
+        if n % i == 0:
+            small_divisors.append(i)
+            if i != n // i:
+                large_divisors.append(n // i)
+    
+    return small_divisors + large_divisors[::-1]
+
+# DP
+for j in range(2, n + 1):
+    divisors = find_divisors(a[j])
+    for i in divisors[::-1]:
+        if i <= n:
+            c[i] = (c[i] + c[i-1]) % mod
+
+ans = 0
+for i in range(1, n + 1):
+    ans = (ans + c[i]) % mod
 
 print(ans)
