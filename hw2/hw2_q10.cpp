7ddeecad78a90f08cf2df88f168afb9c2213d69f
ryokamoi <ryokamoi.jp@gmail.com>
2024-09-28 16:26:50 -0400

    updated hw2/hw2_q10.cpp, still Time limit exceeded on test 5

--- hw2/hw2_q10.cpp
+++ hw2/hw2_q10.cpp
@@ -8,61 +8,58 @@ int main() {
     int t;
     cin >> t;
 
-    vector<pair<int, int>> problems(t);
-    for (int i = 0; i < t; ++i) {
+    vector<pair<int, int>> problems;
+    for (int i = 0; i < t; i++) {
         int a, b;
         cin >> a >> b;
-        problems[i] = make_pair(a, b);
+        problems.push_back({a, b});
     }
 
-    // Set the maximum number
+    // get all prime numbers up to max_num
     int max_num = 5000000;
-
-    // Sieve of Eratosthenes to mark prime numbers
     vector<bool> is_prime(max_num + 1, true);
-    is_prime[0] = is_prime[1] = false;
-    for (int i = 2; i * i <= max_num; ++i) {
+    vector<int> prime_factor(max_num + 1, 0);
+    is_prime[0] = false;
+    is_prime[1] = false;
+    
+    for (int i = 2; i <= sqrt(max_num); i++) {
         if (is_prime[i]) {
+            prime_factor[i] = i;
             for (int j = i * i; j <= max_num; j += i) {
                 is_prime[j] = false;
+                prime_factor[j] = i;
             }
         }
     }
 
-    // Get all prime numbers up to max_num
+    // get all prime numbers up to max_num
     vector<int> prime_numbers;
-    for (int i = 2; i <= max_num; ++i) {
+    for (int i = 0; i <= max_num; i++) {
         if (is_prime[i]) {
             prime_numbers.push_back(i);
         }
     }
 
-    // Count number of prime factors
+    // count num of prime factors
     vector<int> num_prime_factors(max_num + 1, 0);
-    for (int a = 2; a <= max_num; ++a) {
+    for (int a = 2; a <= max_num; a++) {
         if (is_prime[a]) {
             num_prime_factors[a] = 1;
             continue;
         }
-
-        for (int prime : prime_numbers) {
-            if (a % prime == 0) {
-                num_prime_factors[a] = 1 + num_prime_factors[a / prime];
-                break;
-            }
-        }
+        num_prime_factors[a] = 1 + num_prime_factors[a / prime_factor[a]];
     }
 
-    // Sum of all prime factors up to max_num
-    vector<long long> sum_of_prime_factors(max_num + 1, 0);
-    for (int a = 2; a <= max_num; ++a) {
+    // sum of all prime factors up to max_num
+    vector<int> sum_of_prime_factors(max_num + 1, 0);
+    for (int a = 2; a <= max_num; a++) {
         sum_of_prime_factors[a] = sum_of_prime_factors[a - 1] + num_prime_factors[a];
     }
 
-    // Answer the queries
-    for (const auto &p : problems) {
-        int a = p.first;
-        int b = p.second;
+    // process each problem
+    for (const auto& problem : problems) {
+        int a = problem.first;
+        int b = problem.second;
         cout << sum_of_prime_factors[a] - sum_of_prime_factors[b] << endl;
     }
 
--- hw2/hw2_q10.py
+++ hw2/hw2_q10.py
@@ -8,12 +8,17 @@ for _ in range(t):
 # get all prime numbers up to max_num
 max_num = 5000000
 is_prime = [True for _ in range(max_num + 1)]
+prime_factor = [0 for _ in range(max_num + 1)]
 is_prime[0] = False
 is_prime[1] = False
 for i in range(2, int(max_num ** 0.5) + 1):
     if is_prime[i]:
+        prime_factor[i] = i
         for j in range(i * i, max_num + 1, i):
             is_prime[j] = False
+            prime_factor[j] = i
+
+# print("done1")
 
 # get all prime numbers up to max_num
 prime_numbers = [i for i in range(max_num + 1) if is_prime[i]]
@@ -25,11 +30,13 @@ for a in range(2, max_num + 1):
         num_prime_factors[a] = 1
         continue
     
-    for prime in prime_numbers:
-        if a % prime == 0:
-            num_prime_factors[a] = 1 + num_prime_factors[a // prime]
-            break
+    # for prime in prime_numbers:
+    #     if a % prime == 0:
+    #         num_prime_factors[a] = 1 + num_prime_factors[a // prime]
+    #         break
+    num_prime_factors[a] = 1 + num_prime_factors[a // prime_factor[a]]
 
+# print("done2")
 
 # sum of all prime factors up to max_num
 sum_of_prime_factors = [0 for _ in range(max_num + 1)]
